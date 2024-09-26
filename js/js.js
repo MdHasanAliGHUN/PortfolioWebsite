90c54999700239f9d547417bb09769db441091df
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2024-09-26 01:50:02 -0700

    Set dark moon function

--- js/js.js
+++ js/js.js
@@ -18,3 +18,18 @@ window.addEventListener("scroll", () => {
         navLinks.classList.add("bg-white","shadow-sm","bg-opacity-50")
     }
 })
+
+
+document.getElementById("moon").addEventListener('click', () => {
+    const body = document.getElementById("body");
+    if (body.style.backgroundColor === "black") {
+        // If the background is already black, change it back to white
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    } else {
+        // Otherwise, change the background to black and text to white
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    }
+    console.log(body);
+});
