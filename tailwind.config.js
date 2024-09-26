fda1735b6f3f6a042024a42d2ab548b759aa2cde
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2024-09-25 22:49:45 -0700

    My Portfolio Website

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme:{
+        fontFamily : {
+            Ovo : ["Ovo", "serif"],
+            Outfit : ["Outfit", 'sans-serif']
+        },
+        animation :{
+            spin_slow : "spin 5s linear infinite"
+        }
+    }
+}
